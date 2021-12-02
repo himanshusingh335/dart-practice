bd489d195f4fa9e65ad1209e4595b85363860ac5
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2021-12-02 20:16:36 +0530

    update classes.dart

--- bin/classes.dart
+++ bin/classes.dart
@@ -1,73 +1,83 @@
-abstract class _Vehicle{
+abstract class _Vehicle {
   int _wheels;
   String _fuel;
   String _colour;
   String _name;
+
+  void description() {}
 }
 
-class _Description{
-  void description(){
-  }
+abstract class _Description {
+  void description() {}
 }
 
-class FourWheeler extends _Vehicle implements _Description{
-
-  String type ;
+class FourWheeler extends _Vehicle implements _Description {
+  String type;
   String size;
 
-  String get carType{
-    return type ;
+  String get carType {
+    return type;
   }
 
-  String get carSize{
-    return size ;
+  String get carSize {
+    return size;
   }
 
-  set carSize(String sizeName){
+  set carSize(String sizeName) {
     size = sizeName;
   }
 
-
-  set carType(String typeName){
+  set carType(String typeName) {
     type = typeName;
   }
 
-
   @override
-  void description(){
-    print( _name + ' is a vehicle with '+ _wheels.toString() + ' wheels, it runs on ' + _fuel + '.Its colour is ' + _colour );
+  void description() {
+    print(_name +
+        ' is a vehicle with ' +
+        _wheels.toString() +
+        ' wheels, it runs on ' +
+        _fuel +
+        '.Its colour is ' +
+        _colour);
   }
 
-  FourWheeler(){
-    _wheels=4;
-    _fuel= 'Not specified';
-    _colour= 'not specified';
-    _name= 'not specified';
+  FourWheeler() {
+    _wheels = 4;
+    _fuel = 'Not specified';
+    _colour = 'not specified';
+    _name = 'not specified';
   }
-  FourWheeler.prop({String name, String fuel, String colour, this.type }){
-    _fuel=fuel;
-    _colour=colour;
-    _wheels=4;
-    _name=name;
+  FourWheeler.prop({String name, String fuel, String colour, this.type}) {
+    _fuel = fuel;
+    _colour = colour;
+    _wheels = 4;
+    _name = name;
   }
 }
 
-class TwoWheeler extends _Vehicle implements _Description{
-    @override
-  void description(){
-    print( _name + ' is a vehicle with '+ _wheels.toString() + ' wheels, it runs on ' + _fuel + '.Its colour is ' + _colour );
+class TwoWheeler extends _Vehicle implements _Description {
+  @override
+  void description() {
+    print(_name +
+        ' is a vehicle with ' +
+        _wheels.toString() +
+        ' wheels, it runs on ' +
+        _fuel +
+        '.Its colour is ' +
+        _colour);
   }
 
-  TwoWheeler(){
-    _wheels=2;
-    _fuel= 'Not specified';
-    _colour= 'not specified';
-    _name= 'not specified';
+  TwoWheeler() {
+    _wheels = 2;
+    _fuel = 'Not specified';
+    _colour = 'not specified';
+    _name = 'not specified';
   }
-  TwoWheeler.prop(String name, String fuel, String colour){
-    _fuel=fuel;
-    _colour=colour;
-    _wheels=2;
-    _name=name;
+  TwoWheeler.prop(String name, String fuel, String colour) {
+    _fuel = fuel;
+    _colour = colour;
+    _wheels = 2;
+    _name = name;
   }
 }
